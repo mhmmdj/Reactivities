afd42a9201b4a52a77a9d870d4b0e77844439fa6
Mohammad <chamaleki@gmail.com>
2022-09-24 23:48:08 +0330

    End of the Session 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain;
+public class Activity
+{
+	public Guid ID { get; set; }
+	public string? Title { get; set; }
+	public DateTime Date { get; set; }
+	public string? Description { get; set; }
+	public string? Category { get; set; }
+	public string? City { get; set; }
+	public string? Venue { get; set; }
+}
---
+++ Persistence/Migrations/20220924051749_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+	public partial class InitialCreate : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateTable(
+				name: "Activities",
+				columns: colBuilder => new
+				{
+					ID = colBuilder.Column<Guid>(type: "TEXT", nullable: false),
+					Title = colBuilder.Column<string>(type: "TEXT", nullable: true),
+					Date = colBuilder.Column<DateTime>(type: "TEXT", nullable: false),
+					Description = colBuilder.Column<string>(type: "TEXT", nullable: true),
+					Category = colBuilder.Column<string>(type: "TEXT", nullable: true),
+					City = colBuilder.Column<string>(type: "TEXT", nullable: true),
+					Venue = colBuilder.Column<string>(type: "TEXT", nullable: true)
+				},
+				constraints: tabler =>
+				{
+					tabler.PrimaryKey("PK_Activities", x => x.ID);
+				});
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+					name: "Activities");
+		}
+	}
+}
